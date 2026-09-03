f470814d2f130fc3c4a957177918d8a95737f1b7
10017200-code <10017200@casdschools.org>
2026-09-03 15:13:13 +0000

    Bug fixes.

--- signin/src/main.go
+++ signin/src/main.go
@@ -24,14 +24,16 @@ func main() {
   cmd := os.Args[1]
   switch cmd {
     case "login":
-      uname, passwd, hash := ""
+      var uname, passwd, hash string
+      fmt.Println(uname, passwd, hash) //Temp
       //Login code here.
       //Asks for username with fmt.Scanln(&uname)
       //Asks for password (2.0 will try to not echo password) with fmt.Scanln(&passwd)
       //Encrypts passwd with sha256.Sum256() and encoding/hex is used to make it a string to input to the xml
       //Checks username first then checks passwd hash with stored hash for said user
     case "signup":
-      uname, passwd, hash := ""
+      var uname, passwd, hash string
+      fmt.Println(uname, passwd, hash) //Temp
       // Same thing as login but instead of reading stores it in the .xml password document
       // Exits program after and tells user to log in with their credentials by running the program again
 
